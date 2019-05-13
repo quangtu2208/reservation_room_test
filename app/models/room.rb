1341209521d5454e9cdfd299ea035302648b1049
QUANG TU <quangtu2208@gmail.com>
2019-05-13 15:41:25 +0700

    user_manager_room_mine

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,6 @@
 class Room < ApplicationRecord
+  include RoomDecorator
+
   has_many :services
   has_many :services, through: :rooms_services
   has_many :images, as: :imageable, dependent: :destroy
@@ -6,4 +8,17 @@ class Room < ApplicationRecord
   has_many :reservations, through: :reservation_details
   belongs_to :location
   belongs_to :bed_detail
+
+  validates_presence_of :name, :occupancy_limit, :living_room, :bed_room,
+    :bath_room, :number_of_bed, :quantity
+  accepts_nested_attributes_for :services
+
+  ROOM_PARAMS = [:location, :name, :occupancy_limit, :living_room, :bed_room,
+    :bath_room, :number_of_bed, :quantity, :price, :location_id, :bed_detail_id,
+    :created_at, :updated_at].freeze
+
+  scope :by_location_id, -> (location_id){ where("location_id = ?", location_id) }
+  scope :list, ->{select :id, :name, :occupancy_limit, :living_room, :bed_room,
+    :bath_room, :number_of_bed, :quantity, :price, :location_id, :created_at,
+    :updated_at}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_09_020020) do
+ActiveRecord::Schema.define(version: 2019_05_14_010130) do
 
   create_table "bed_details", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
